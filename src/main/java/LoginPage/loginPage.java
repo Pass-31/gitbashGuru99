32557a765ef002c62e90809728eb0493c9978bc3
unknown <mdeokate31@gmail.com>
2025-07-05 23:03:56 +0530

    framework created

---
+++ src/main/java/LoginPage/loginPage.java
@@ -0,0 +1,9 @@
+package LoginPage;
+
+public class loginPage {
+	public static void loginfunctionality () {
+		System.out.println("login sucesfully");
+		
+	}
+
+}
---
+++ src/test/java/LoginPageTest/loginpagetest.java
@@ -0,0 +1,14 @@
+package LoginPageTest;
+
+import LoginPage.loginPage;
+
+public class loginpagetest {
+
+	public static void main(String[] args) {
+		
+		loginPage obj1 = new loginPage();
+		obj1.loginfunctionality();		
+
+	}
+
+}
