b5611166cbd43679d71272b03cac38b7d7ad83ab
unknown <mdeokate31@gmail.com>
2025-07-05 23:36:00 +0530

    HomePage functionality created

---
+++ src/test/java/LoginPageTest/HomePageTest.java
@@ -0,0 +1,19 @@
+package LoginPageTest;
+
+import LoginPage.HomePage;
+import LoginPage.loginPage;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+
+
+		loginPage obj1 = new loginPage();
+		obj1.loginfunctionality();	
+		
+		HomePage obj2 = new HomePage();
+		obj2.homepagefunctionality();	
+
+	}
+
+}
